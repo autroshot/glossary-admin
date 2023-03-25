34c8196f35a0a89480ede75e6eeaf3ea8ebdcddf
autroshot <autroshot@gmail.com>
2023-03-26 08:04:51 +0900

    디자인(홈 페이지): 본문 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Container, Heading } from '@chakra-ui/react';
 import Head from 'next/head';
 
 export default function Home() {
@@ -7,9 +8,9 @@ export default function Home() {
         <title>홈 | 용어집 관리자</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>홈페이지</div>
-      </main>
+      <Container>
+        <Heading size="md">용어집 관리자에 오신 것을 환영합니다!</Heading>
+      </Container>
     </>
   );
 }
