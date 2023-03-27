83ad7ac074ad2c97778a9c13dfc67c90bc8b976b
autroshot <autroshot@gmail.com>
2023-03-27 13:17:32 +0900

    기능(구글 용어집 페이지): 용어집 생성 버튼에 테스트용 처리기 구현

--- pages/google-glossary.tsx
+++ pages/google-glossary.tsx
@@ -1,12 +1,19 @@
+import { createGlossary } from '@/fetchers';
 import { Button, Container, Heading } from '@chakra-ui/react';
 
 export default function GoogleGlossary() {
   return (
     <Container>
       <Heading>구글 용어집</Heading>
-      <Button mt="3" onClick={() => console.log('용어집 생성')}>
+      <Button mt="3" onClick={handleCreateGlossaryButtonClick}>
         용어집 생성
       </Button>
     </Container>
   );
+
+  async function handleCreateGlossaryButtonClick(): Promise<void> {
+    const res = await createGlossary();
+
+    console.log(res);
+  }
 }
