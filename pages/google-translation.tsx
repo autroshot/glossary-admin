0f8d3c27bef1b429f641421e13acab8185966d0f
autroshot <autroshot@gmail.com>
2023-03-27 12:46:05 +0900

    디자인(구글 번역 페이지): 머리말 텍스트 변경

--- pages/google-translation.tsx
+++ pages/google-translation.tsx
@@ -3,7 +3,7 @@ import { Button, Container, Heading } from '@chakra-ui/react';
 export default function GoogleTranslation() {
   return (
     <Container>
-      <Heading>구글 번역</Heading>
+      <Heading>구글 용어집</Heading>
       <Button mt="3" onClick={() => console.log('용어집 생성')}>
         용어집 생성
       </Button>
