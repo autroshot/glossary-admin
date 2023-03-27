9bde9dfffb47099cfad0c37be43a80621e0dae50
autroshot <autroshot@gmail.com>
2023-03-27 23:59:11 +0900

    리팩터링(구글 용어집 API): 일부 논리 controllerSwitch 함수 사용으로 대체

--- pages/api/google-glossaries.ts
+++ pages/api/google-glossaries.ts
@@ -1,3 +1,4 @@
+import { controllerSwitch } from '@/utils/api';
 import { TranslationServiceClient } from '@google-cloud/translate';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
@@ -7,52 +8,51 @@ export default async function handler(
 ) {
   const parent = `projects/${process.env.GOOGLE_PROJECT_NUMBER}/locations/us-central1`;
 
-  if (req.method === 'POST') {
-    req.body;
+  await controllerSwitch(req, res, {
+    POSTController: async () => {
+      const client = new TranslationServiceClient({
+        credentials: {
+          client_email: process.env.GOOGLE_CLIENT_EMAIL,
+          private_key: String(process.env.GOOGLE_PRIVATE_KEY).replace(
+            /\\n/gm,
+            '\n'
+          ),
+        },
+      });
 
-    const client = new TranslationServiceClient({
-      credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: String(process.env.GOOGLE_PRIVATE_KEY).replace(
-          /\\n/gm,
-          '\n'
-        ),
-      },
-    });
-
-    const result = await client.createGlossary({
-      parent,
-      glossary: {
-        name: `projects/${process.env.GOOGLE_PROJECT_NUMBER}/locations/us-central1/glossaries/my-glossary2`,
-        languagePair: { sourceLanguageCode: 'en', targetLanguageCode: 'ko' },
-        inputConfig: {
-          gcsSource: {
-            inputUri: process.env.GOOGLE_GLOSSARY_INPUT_URI,
+      const result = await client.createGlossary({
+        parent,
+        glossary: {
+          name: `projects/${process.env.GOOGLE_PROJECT_NUMBER}/locations/us-central1/glossaries/my-glossary2`,
+          languagePair: { sourceLanguageCode: 'en', targetLanguageCode: 'ko' },
+          inputConfig: {
+            gcsSource: {
+              inputUri: process.env.GOOGLE_GLOSSARY_INPUT_URI,
+            },
           },
         },
-      },
-    });
-    console.log(result);
+      });
+      console.log(result);
 
-    res.status(200).json({ message: '용어집 생성 완료!' });
-  } else if (req.method === 'GET') {
-    const client = new TranslationServiceClient({
-      credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: String(process.env.GOOGLE_PRIVATE_KEY).replace(
-          /\\n/gm,
-          '\n'
-        ),
-      },
-    });
+      res.status(200).json({ message: '용어집 생성 완료!' });
+    },
+    GETController: async () => {
+      const client = new TranslationServiceClient({
+        credentials: {
+          client_email: process.env.GOOGLE_CLIENT_EMAIL,
+          private_key: String(process.env.GOOGLE_PRIVATE_KEY).replace(
+            /\\n/gm,
+            '\n'
+          ),
+        },
+      });
 
-    const queryResult = await client.listGlossaries({
-      parent,
-    });
-    const glossaries = queryResult[0];
+      const queryResult = await client.listGlossaries({
+        parent,
+      });
+      const glossaries = queryResult[0];
 
-    res.status(200).json({ data: glossaries });
-  } else {
-    res.status(405).end();
-  }
+      res.status(200).json({ data: glossaries });
+    },
+  });
 }
