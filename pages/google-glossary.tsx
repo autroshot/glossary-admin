46244a0c67a74e78bac22d493b9bb6da9f90dc80
autroshot <autroshot@gmail.com>
2023-03-28 19:38:24 +0900

    디자인(구글 용어집 페이지): '용어집들 읽기' 버튼 제거

--- pages/google-glossary.tsx
+++ pages/google-glossary.tsx
@@ -1,21 +1,11 @@
 import GlossaryTable from '@/components/glossary-table';
-import { getGlossaries } from '@/fetchers';
-import { Box, Button, Container, Heading } from '@chakra-ui/react';
+import { Container, Heading } from '@chakra-ui/react';
 
 export default function GoogleGlossary() {
   return (
     <Container>
       <Heading>구글 용어집</Heading>
-      <Box mt="3">
-        <Button onClick={handleGetGlossariesButtonClick}>용어집들 읽기</Button>
-      </Box>
       <GlossaryTable />
     </Container>
   );
-
-  async function handleGetGlossariesButtonClick(): Promise<void> {
-    const res = await getGlossaries();
-
-    console.log(res);
-  }
 }
