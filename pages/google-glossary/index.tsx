7dd0dbbd91f1e211fb896214f75f7a2f05fe4b6e
autroshot <autroshot@gmail.com>
2023-03-28 20:31:42 +0900

    리팩터링: 구글 용어집 목록 페이지의 함수명 변경

--- pages/google-glossary/index.tsx
+++ pages/google-glossary/index.tsx
@@ -1,7 +1,7 @@
 import GlossaryTable from '@/components/google-glossary/glossary-table';
 import { Container, Heading } from '@chakra-ui/react';
 
-export default function GoogleGlossary() {
+export default function GoogleGlossaryList() {
   return (
     <Container>
       <Heading>구글 용어집</Heading>
