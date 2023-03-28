2e5368647e815bf685a2ff924a088f6bd3536cbe
autroshot <autroshot@gmail.com>
2023-03-28 18:30:46 +0900

    디자인(구글 용어집 페이지): 표 변경

--- components/glossary-table.tsx
+++ components/glossary-table.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Table,
   TableCaption,
@@ -9,39 +10,42 @@ import {
   Thead,
   Tr,
 } from '@chakra-ui/react';
-import styles from './glossary-table.module.css';
 
 export default function GlossaryTable() {
   return (
     <TableContainer>
-      <Table variant="simple" maxW="100%" className={styles.table}>
+      <Table variant="simple">
         <TableCaption placement="top" fontSize="lg" fontWeight="bold">
           용어집 목록
         </TableCaption>
         <Thead>
           <Tr>
-            <Th w="80%">이름</Th>
-            <Th w="20%" textAlign="center">
-              상세
-            </Th>
+            <Th>이름</Th>
+            <Th textAlign="center">상세</Th>
           </Tr>
         </Thead>
         <Tbody>
           <Tr>
-            <Td>my-glossary-1</Td>
+            <Td maxW="20rem" overflow="hidden" textOverflow="ellipsis">
+              my-glossary-1
+            </Td>
             <Td padding="0" textAlign="center">
               <Button size="sm">보기</Button>
             </Td>
           </Tr>
           <Tr>
-            <Td>my-glossary-2</Td>
+            <Td maxW="20rem" overflow="hidden" textOverflow="ellipsis">
+              my-glossary-2
+            </Td>
             <Td padding="0" textAlign="center">
               <Button size="sm">보기</Button>
             </Td>
           </Tr>
           <Tr>
-            <Td overflow="hidden" textOverflow="ellipsis">
-              my-glossary-3my-glossary-3my-glossary-3my-glossary-3my-glossary-3my-glossary-3
+            <Td>
+              <Box maxW="20rem" overflow="hidden" textOverflow="ellipsis">
+                my-glossary-3
+              </Box>
             </Td>
             <Td padding="0" textAlign="center">
               <Button size="sm">보기</Button>
