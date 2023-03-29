fea40f005a11b537badaf1fb89abaabc26a7ae59
autroshot <autroshot@gmail.com>
2023-03-29 13:55:07 +0900

    리팩터링(구글 용어집): 구글 API에서 용어집 목록을 가져오는 논리 변경
    구글 클라이언트 -> 구글 인증 라이브러리 + REST

--- controllers/google-glossary/index.ts
+++ controllers/google-glossary/index.ts
@@ -1,16 +1,28 @@
 import { Controller } from '@/types/controller';
-import { PARENT } from './constants';
-import { createTranslationServiceClient } from './utils';
+import { Glossary } from '@/types/models';
+import { JWT } from 'google-auth-library';
 
 const getGlossaries: Controller = async (req, res) => {
-  const client = createTranslationServiceClient();
-
-  const queryResult = await client.listGlossaries({
-    parent: PARENT,
+  const client = new JWT({
+    email: process.env.GOOGLE_CLIENT_EMAIL,
+    key: String(process.env.GOOGLE_PRIVATE_KEY).replace(/\\n/gm, '\n'),
+    scopes: [
+      'https://www.googleapis.com/auth/cloud-platform',
+      'https://www.googleapis.com/auth/cloud-translation',
+    ],
   });
-  const glossaries = queryResult[0];
+
+  const googleAPIresponse =
+    await client.request<GetGlossariesGoogleAPIResponse>({
+      url: `https://translate.googleapis.com/v3/projects/${process.env.GOOGLE_PROJECT_NUMBER}/locations/us-central1/glossaries`,
+    });
+  const glossaries = googleAPIresponse.data.glossaries;
 
   return res.status(200).json({ data: glossaries });
 };
+
+interface GetGlossariesGoogleAPIResponse {
+  glossaries: Glossary[];
+}
 
 export { getGlossaries };
