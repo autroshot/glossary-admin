987a19bc756f8d5aafba7db52d45d9799b36ff27
autroshot <autroshot@gmail.com>
2023-03-29 14:01:56 +0900

    리팩터링(구글 용어집 컨트롤러): 미사용 함수 제거

--- controllers/google-glossary/utils.ts
+++ controllers/google-glossary/utils.ts
@@ -1,17 +1,4 @@
-import { TranslationServiceClient } from '@google-cloud/translate';
 import { JWT } from 'google-auth-library';
-
-export function createTranslationServiceClient(): TranslationServiceClient {
-  return new TranslationServiceClient({
-    credentials: {
-      client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: String(process.env.GOOGLE_PRIVATE_KEY).replace(
-        /\\n/gm,
-        '\n'
-      ),
-    },
-  });
-}
 
 export function createJWTClient(): JWT {
   return new JWT({
