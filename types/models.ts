d9fc6f6d6c73c6bcfecdf25199ae8b803881e759
autroshot <autroshot@gmail.com>
2023-03-29 18:50:41 +0900

    리팩터링(구글 용어집): 모델에 타입 추가

--- types/models.ts
+++ types/models.ts
@@ -15,6 +15,11 @@ export interface GoogleGlossary {
   };
 }
 
+export interface MyGoogleGlossary {
+  path: string;
+  name: string;
+}
+
 export interface GoogleGlossaryEntry {
   name: string;
   description: string;
@@ -22,6 +27,15 @@ export interface GoogleGlossaryEntry {
     sourceTerm: GlossaryTerm;
     targetTerm: GlossaryTerm;
   };
+}
+
+export interface MyGoogleTerm extends Term {
+  path: string;
+}
+
+interface Term {
+  english: string;
+  korean: string;
 }
 
 interface GlossaryTerm {
