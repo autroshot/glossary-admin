2b29e45f42412a48a1b51c327520e5b1589daf98
autroshot <autroshot@gmail.com>
2023-03-30 02:01:34 +0900

    버그: createTerm 컨트롤러의 잘못된 옵션
    body -> data

--- controllers/google-glossary/term.ts
+++ controllers/google-glossary/term.ts
@@ -10,7 +10,7 @@ const createTerm: Controller = async (req, res) => {
   const googleAPIResponse = await client.request<GoogleAPICreateResponse>({
     url: `https://translate.googleapis.com/v3/projects/${process.env.GOOGLE_PROJECT_NUMBER}/locations/us-central1/glossaries/${glossaryName}/glossaryEntries`,
     method: 'POST',
-    body: toPostRequestBody(receivedBody),
+    data: toPostRequestBody(receivedBody),
   });
 
   if (googleAPIResponse.data) return res.status(200).end();
