bb5db41656780b9a8e2a2ec829a5a674747257a5
autroshot <autroshot@gmail.com>
2023-03-29 14:05:30 +0900

    리팩터링(구글 용어집 컨트롤러): 상수 인라인화. 상수명 변경

--- controllers/google-glossary/index.ts
+++ controllers/google-glossary/index.ts
@@ -5,13 +5,12 @@ import { createJWTClient } from './utils';
 const getGlossaries: Controller = async (req, res) => {
   const client = createJWTClient();
 
-  const googleAPIresponse =
+  const googleAPIResponse =
     await client.request<GetGlossariesGoogleAPIResponse>({
       url: `https://translate.googleapis.com/v3/projects/${process.env.GOOGLE_PROJECT_NUMBER}/locations/us-central1/glossaries`,
     });
-  const glossaries = googleAPIresponse.data.glossaries;
 
-  return res.status(200).json({ data: glossaries });
+  return res.status(200).json({ data: googleAPIResponse.data.glossaries });
 };
 
 interface GetGlossariesGoogleAPIResponse {
