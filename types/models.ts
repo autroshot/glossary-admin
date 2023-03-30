3713a877b50bdd565fc7fae13f8e4b2b1053e6f1
autroshot <autroshot@gmail.com>
2023-03-30 16:31:58 +0900

    리팩터링(구글 용어집): 타입에 주석 추가

--- types/models.ts
+++ types/models.ts
@@ -1,8 +1,12 @@
+// https://cloud.google.com/translate/docs/reference/rest/v3/projects.locations.glossaries
 export interface MyGoogleGlossary {
+  // displayName
   id: string;
 }
 
+// https://cloud.google.com/translate/docs/reference/rest/v3/projects.locations.glossaries.glossaryEntries
 export interface MyGoogleTerm extends Term {
+  // name에서 마지막 / 뒤의 문자열
   index: string;
 }
 
