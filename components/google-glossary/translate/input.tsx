efba89aaa5d15c6021db1bea5213bab952a309b8
autroshot <autroshot@gmail.com>
2023-03-31 16:22:49 +0900

    디자인(구글 용어집 번역하기 페이지): 리액트 훅 폼 적용

--- components/google-glossary/translate/input.tsx
+++ components/google-glossary/translate/input.tsx
@@ -1,21 +1,41 @@
 import { Box, Button, Heading, Textarea } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
 
 export default function Input() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
   return (
     <Box>
       <Heading as="h3" size="md">
         영어
       </Heading>
-      <Box mt="3">
-        <Textarea
-          rows={10}
-          placeholder="번역할 내용을 입력하세요."
-          resize="none"
-        />
-      </Box>
-      <Box mt="3">
-        <Button>번역하기</Button>
-      </Box>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box mt="3">
+          <Textarea
+            {...register('english', { required: true })}
+            isInvalid={Boolean(errors.english?.message)}
+            rows={10}
+            placeholder="번역할 내용을 입력하세요."
+            resize="none"
+          />
+        </Box>
+        <Box mt="3">
+          <Button type="submit">번역하기</Button>
+        </Box>
+      </form>
     </Box>
   );
+
+  function onSubmit(data: Inputs): unknown | Promise<unknown> {
+    console.log(data);
+    return;
+  }
+
+  interface Inputs {
+    english: string;
+  }
 }
