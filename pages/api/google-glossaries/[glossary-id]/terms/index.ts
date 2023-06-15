75f233ddcec00f5d0c8ab334641d57dfaa4f89e5
autroshot <autroshot@gmail.com>
2023-06-15 14:38:12 +0900

    리팩터링(구글 용어집): 미사용 API 제거

--- pages/api/google-glossaries/[glossary-id]/terms/index.ts
+++ pages/api/google-glossaries/[glossary-id]/terms/index.ts
@@ -1,12 +1,7 @@
-import {
-  createTerm,
-  deleteTerm,
-  getTerms,
-} from '@/controllers/google-glossary/term';
+import { createTerm, getTerms } from '@/controllers/google-glossary/term';
 import { handler } from '@/utils/api';
 
 export default handler({
   POSTController: createTerm,
   GETController: getTerms,
-  DELETEController: deleteTerm,
 });
